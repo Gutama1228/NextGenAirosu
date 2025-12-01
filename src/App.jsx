19530d1ca33cbc107b5096702e66147d5b9c8eba
gutama <142733438+Gutama1228@users.noreply.github.com>
2025-12-02 00:37:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './routes/AppRouter';
+import { AuthProvider } from './context/AuthContext';
+import { ChatProvider } from './context/ChatContext';
+import './App.css';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <ChatProvider>
+          <AppRouter />
+        </ChatProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
