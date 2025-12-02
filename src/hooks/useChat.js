7b16509c42e1c900229fe768e82aacd360ade1fb
gutama <142733438+Gutama1228@users.noreply.github.com>
2025-12-03 01:40:43 +0700

    Update useChat.js

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,1 +1,19 @@
+import { useContext } from 'react';
+import { ChatContext } from '../context/ChatContext';
 
+/**
+ * Custom hook to use Chat Context
+ * @returns {Object} Chat context value
+ * @throws {Error} If used outside ChatProvider
+ */
+export const useChat = () => {
+  const context = useContext(ChatContext);
+
+  if (!context) {
+    throw new Error('useChat must be used within a ChatProvider');
+  }
+
+  return context;
+};
+
+export default useChat;
